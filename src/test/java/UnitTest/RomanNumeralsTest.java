c7975dd22a52410f889f2c26ca8f675858eb811a
vinay <vinaykris@gmail.com>
2017-12-11 20:26:56 +0530

    Make changes in unit test

--- src/test/java/UnitTest/RomanNumeralsTest.java
+++ src/test/java/UnitTest/RomanNumeralsTest.java
@@ -7,12 +7,15 @@ import cicd_workshop.RomanNumerals;
 public class RomanNumeralsTest {
 	@Test
 	public void should_return_I_when_input_1(){
+		//arrange
 		RomanNumerals converter = new RomanNumerals();
 		int inputNumber = 1;
 		String expectedResult = "I";
 		
+		//act
 		String actualResult = converter.convert(inputNumber);
 		
+		//assert
 		Assert.assertEquals(expectedResult, actualResult);
 	}
 }
